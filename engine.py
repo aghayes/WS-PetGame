54ce6566b30c43ba880a32f398548a334db03046
C-Jameson <booksncode@gmail.com>
2019-10-09 11:02:17 -0400

    added recording and fixed the doubling up

--- engine.py
+++ engine.py
@@ -12,14 +12,18 @@ clip = sound_manager.concatenate_audio(clip_array)
 sound_manager.play(clip, '1')
 
 question1 = question_box.order([scenes[0].events[0], scenes[0].events[1]])
-s_answer = input(":")
-if s_answer == question1:
-    print("good job")
-else:
-    print("sorry")
+# s_answer = input(":")
+# if s_answer == question1:
+#     print("good job")
+# else:
+#     print("sorry")
+print("record your answer!")
+sound_manager.record_audio(5, 'answer1.wav')
+
 new_array = sound_manager.open_audio([scenes[1].events[1][0], scenes[1].events[0][0]])
 clip2 = sound_manager.concatenate_audio(new_array)
 sound_manager.play(clip2, '2')
+
 question2 = question_box.order([scenes[1].events[1], scenes[1].events[0]])
 s_answer = input(":")
 if s_answer == question2:
--- questions.py
+++ questions.py
@@ -15,7 +15,11 @@ class Questions:
     def order(self, ordered_events):
 
         event_choices = random.sample(ordered_events, 2)
-        random.shuffle(event_choices)
+        print(event_choices)
+        for i in range(random.randint(0, 10)):
+            random.shuffle(event_choices)
+        print(event_choices)
+
         first_index = ordered_events.index(event_choices[0])
         second_index = ordered_events.index(event_choices[1])
         self.engine.say("Did the " + event_choices[0][1] + " before or after the " + event_choices[1][2])
