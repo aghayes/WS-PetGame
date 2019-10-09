de6465db863544a01ebdae4a250c9558e8036296
C-Jameson <booksncode@gmail.com>
2019-10-09 11:17:59 -0400

    added some comments in order to document code

--- engine.py
+++ engine.py
@@ -12,21 +12,15 @@ clip = sound_manager.concatenate_audio(clip_array)
 sound_manager.play(clip, '1')
 
 question1 = question_box.order([scenes[0].events[0], scenes[0].events[1]])
-# s_answer = input(":")
-# if s_answer == question1:
-#     print("good job")
-# else:
-#     print("sorry")
-print("record your answer!")
-sound_manager.record_audio(5, 'answer1.wav')
+print("Record your answer!")
+sound_manager.record_audio(7, 'answer1.wav')
+print("Good job.")
 
 new_array = sound_manager.open_audio([scenes[1].events[1][0], scenes[1].events[0][0]])
 clip2 = sound_manager.concatenate_audio(new_array)
 sound_manager.play(clip2, '2')
 
 question2 = question_box.order([scenes[1].events[1], scenes[1].events[0]])
-s_answer = input(":")
-if s_answer == question2:
-    print("good job")
-else:
-    print("sorry")
+print("Record your answer!")
+sound_manager.record_audio(7, 'answer2.wav')
+print("Good job.")
--- questions.py
+++ questions.py
@@ -6,6 +6,7 @@ class Questions:
 
     def __init__(self):
 
+        # initialize engine for pyttsx3 which handles speech synth
         self.engine = pyttsx3.init()
         rate = self.engine.getProperty('rate')
         self.engine.setProperty('rate', rate-50)
@@ -14,16 +15,20 @@ class Questions:
 
     def order(self, ordered_events):
 
+        # takes two random events from the ordered list and saves them to event_choices
         event_choices = random.sample(ordered_events, 2)
-        print(event_choices)
+        # shuffles the event_choices array a random number of times between 0 and 10 to create a good random order
+        # for some reason simply calling random.shuffle didn't seem random.
         for i in range(random.randint(0, 10)):
             random.shuffle(event_choices)
-        print(event_choices)
 
+        # checks where the events in event_choices where in the ordered list of events in order to get event chronology
         first_index = ordered_events.index(event_choices[0])
         second_index = ordered_events.index(event_choices[1])
+        # asks the question
         self.engine.say("Did the " + event_choices[0][1] + " before or after the " + event_choices[1][2])
         self.engine.runAndWait()
+        # checks the chronology and returns the answer based on it
         if first_index > second_index:
             self.answer = "after"
         else:
