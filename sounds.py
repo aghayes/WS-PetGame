ddb58b2052d9e5e505f7bc63ba25337da617c336
C-Jameson <booksncode@gmail.com>
2019-10-21 14:48:09 -0400

    Revert "fixed merge issue"
    This reverts commit 1bda49ed0b5be1ac2bbbe117440b1068f2323b75, reversing
    changes made to 4d1579906baf1bdbc3dfb44f9866e8f9e5c558a6.

--- sounds.py
+++ sounds.py
@@ -1,23 +1,22 @@
 from pydub import *
 from pydub.playback import *
-import sounddevice
-import scipy.io.wavfile
+
 
 class SoundManager:
 
+    file_paths = []
+    audio_clips = []
 
-    @staticmethod
-    def open_audio(file_path_array):
-        file_paths = file_path_array
-        audio_clips = []
-        for path in file_paths:
+    def open_audio(self, file_path_array):
+        self.file_paths = file_path_array
+
+        for path in self.file_paths:
             extension = path.split('.')[1]
-            audio_clips.append(AudioSegment.from_file(path, extension))
+            self.audio_clips.append(AudioSegment.from_file(path, extension))
 
-        return audio_clips
+        return self.audio_clips
 
-    @staticmethod
-    def concatenate_audio(audio_clip_array):
+    def concatenate_audio(self, audio_clip_array):
         final_clip = 0
         for clip in audio_clip_array:
             final_clip = final_clip + clip
@@ -27,10 +26,3 @@ class SoundManager:
     def play(audio_clip, clip_number):
         play(audio_clip)
         audio_clip.export("clip{}.mp3".format(clip_number), 'mp3')
-
-    @staticmethod
-    def record_audio(record_length, output_file_path):
-        fs = 44100
-        recording = sounddevice.rec(int(record_length*fs), fs, channels=2)
-        sounddevice.wait()
-        scipy.io.wavfile.write(output_file_path, fs, recording)
