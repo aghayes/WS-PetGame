4d1579906baf1bdbc3dfb44f9866e8f9e5c558a6
C-Jameson <booksncode@gmail.com>
2019-10-21 14:41:30 -0400

    changed something

--- engine.py
+++ engine.py
@@ -12,7 +12,7 @@ clip = sound_manager.concatenate_audio(clip_array)
 sound_manager.play(clip, '1')
 
 question1 = question_box.order([scenes[0].events[0], scenes[0].events[1]])
-s_answer = input(":")
+s_answer = input("Before or after:")
 if s_answer == question1:
     print("good job")
 else:
@@ -21,7 +21,7 @@ new_array = sound_manager.open_audio([scenes[1].events[1][0], scenes[1].events[0
 clip2 = sound_manager.concatenate_audio(new_array)
 sound_manager.play(clip2, '2')
 question2 = question_box.order([scenes[1].events[1], scenes[1].events[0]])
-s_answer = input(":")
+s_answer = input("Before or after:")
 if s_answer == question2:
     print("good job")
 else:
